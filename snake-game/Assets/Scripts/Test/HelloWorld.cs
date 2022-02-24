ac9a5a06865c0e586f3c69b0dc22f504797fa464
Kristofer Sell <kristoferelissell@gmail.com>
2022-02-24 11:30:59 +0100

    Added base stuff
    Added folders, implemented new textures and did a initial test with script

---
+++ snake-game/Assets/Scripts/Test/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace elikrisel
+{
+
+    public class HelloWorld : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+
+
+
+}
